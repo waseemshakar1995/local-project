b4616f1028f8f800ecb37201f30ad7a46165895d
asad kamran shah <asad.shah@fauji.org.pk>
2023-08-04 12:47:55 +0500

    Testing: today token

--- src/app/Service/auth-interceptors.service.ts
+++ src/app/Service/auth-interceptors.service.ts
@@ -1,18 +1,21 @@
-
-import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest, } from "@angular/common/http";
+import {
+  HttpEventType,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 
 export class AuthInterceptorService implements HttpInterceptor {
-    intercept(req: HttpRequest<any>, next: HttpHandler) {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const token =
+      'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJsYUp3dElkIiwiaWF0IjoxNjkxMTMzNTI1LCJzdWIiOiIzMTM2MSIsImlzcyI6ImxhSnd0SXNzdWVyIiwiZW1wbnVtIjoiMV8xIiwiaXNhZG1pbiI6IlkiLCJzdWJjbyI6IjEiLCJmdWxsbmFtZSI6IlVzYW1hIEFsaSAiLCJ1c2VyTmFtZSI6InVhIiwidXNlcmlkIjoiMzEzNjEiLCJ1c2VyQ29kZSI6IjMxMzYxIiwidXNlcmVtYWlsIjoiaW5mby51c2FtYWFsaUBnbWFpbC5jb20iLCJleHAiOjE2OTExODc1MjV9.Mp5OdOsiYuh2y2zr0SfPPhNQtrQh33nl8kcjOKk0tXg';
 
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJsYUp3dElkIiwiaWF0IjoxNjkxMDQ2MTU2LCJzdWIiOiIzMTM2MSIsImlzcyI6ImxhSnd0SXNzdWVyIiwiZW1wbnVtIjoiMV8xIiwiaXNhZG1pbiI6IlkiLCJzdWJjbyI6IjEiLCJmdWxsbmFtZSI6IlVzYW1hIEFsaSAiLCJ1c2VyTmFtZSI6InVhIiwidXNlcmlkIjoiMzEzNjEiLCJ1c2VyQ29kZSI6IjMxMzYxIiwidXNlcmVtYWlsIjoiaW5mby51c2FtYWFsaUBnbWFpbC5jb20iLCJleHAiOjE2OTExMDAxNTZ9.9_GOdSbmeVBIHnyp-PKuG2pknEpwMj6Gio8Ead7OjTM'
+    let modifiedRequest = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-        let modifiedRequest = req.clone({
-
-            setHeaders: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-
-        return next.handle(modifiedRequest);
-}
+    return next.handle(modifiedRequest);
+  }
 }
